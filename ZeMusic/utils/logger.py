eb236bc711a870f88c8a3733c66c073884989582
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-09 09:34:20 +0200

    Update logger.py

--- ZeMusic/utils/logger.py
+++ ZeMusic/utils/logger.py
@@ -1,28 +1,29 @@
 from pyrogram.enums import ParseMode
+
 from ZeMusic import app
 from ZeMusic.utils.database import is_on_off
 from config import LOGGER_ID
 
+
 async def play_logs(message, streamtype):
     if await is_on_off(2):
-        chatusername = await app.get_chat(message.chat.id).username
-        logger_text = f"""━━━━━━━━━━━━━━━
-◍ [⌞ 𝘾𝙍 𖢻 ⌯ 𝙈𝙐𝙎𝙄𝘾 ⌯ 🎧💚   
-━━━━━━━━━━━━━━━
-🌹️ اسم المجموعة : > {message.chat.title} [{message.chat.id}]
-━━━━━━━━━━━━━━━
-🥀 اسم المستخدم : › {message.from_user.mention}
-━━━━━━━━━━━━━━━
-🌸 يوزر المستخدم : › @{message.from_user.username}
-━━━━━━━━━━━━━━━
-🌷 ايدي المستخدم  : › {message.from_user.id}
-━━━━━━━━━━━━━━━
-🌿 رابط الجروب: > {chatusername}
-━━━━━━━━━━━━━━━
-🌻 المطلوب: {message.text}
-━━━━━━━━━━━━━━━
-💐 نوع التشغيل: {streamtype}
-━━━━━━━━━━━━━━━"""
+        logger_text = f"""
+ٴ<b>•────‌‌‏──‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏─•</b>
+<b>إشعـارات  الميـوزك 𝄞</b>
+ٴ<b>•────‌‌‏─‌‌‏─‌‌‏─‌‌‏─‌‌‏──‌‌‏─‌‌‏─•</b>
+<b>- سيـدي المطـور</b>
+<b>- هنـاك شخص يستخـدم الميـوزك حاليـاً</b>
+
+<b>- الاسم :</b> {message.from_user.mention}
+<b>- اليوزر :</b> @{message.from_user.username}
+<b>- ايدي المستخدم :</b> <code>{message.from_user.id}</code>
+
+<b>- اسم المجموعة :</b> {message.chat.title}
+<b>- يوزر المجموعة :</b> @{message.chat.username}
+<b>- ايدي المجموعة :</b> <code>{message.chat.id}</code>
+
+<b>- الطلب :</b> {message.text.split(None, 1)[1]}
+<b>- نوع التشغيل :</b> {streamtype}"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
@@ -31,5 +32,6 @@ async def play_logs(message, streamtype):
                     parse_mode=ParseMode.HTML,
                     disable_web_page_preview=True,
                 )
-            except Exception as e:
-                print(f"Error sending message: {e}")
+            except:
+                pass
+        return
