ef09a7936816b434989aef0f701c1e7636e1d2cb
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-09 09:18:16 +0200

    Update logger.py

--- ZeMusic/utils/logger.py
+++ ZeMusic/utils/logger.py
@@ -1,29 +1,29 @@
 from pyrogram.enums import ParseMode
 
-from ZeMusic import app
-from ZeMusic.utils.database import is_on_off
+from MatrixMusic import app
+from MatrixMusic.utils.database import is_on_off
 from config import LOGGER_ID
 
 
 async def play_logs(message, streamtype):
     if await is_on_off(2):
-        logger_text = f"""**━━━━━━━━━━━━━━━**
-****◍ [⌞ 𝘾𝙍 𖢻 ⌯ 𝙈𝙐𝙎𝙄𝘾 ⌯ ˹🎧˼⁩ ⌝****
-**━━━━━━━━━━━━━━━**
-**🌹️ اسم المجموعة : >** {message.chat.title} [`{message.chat.id}`]
-**━━━━━━━━━━━━━━━**
-**🥀 اسم المستخدم : ›** {message.from_user.mention}
-**━━━━━━━━━━━━━━━**
-**🌸 يوزر المستخدم : ›** @{message.from_user.username}
-**━━━━━━━━━━━━━━━**
-**🌷 ايدي المستخدم  : ›** `{message.from_user.id}`
-**━━━━━━━━━━━━━━━**
-**🌿 رابط الجروب: >** {chatusername}
-**━━━━━━━━━━━━━━━**
-**🌻 المطلوب:** {message.text}
-**━━━━━━━━━━━━━━━**
-**💐 نوع التشغيل:** {streamtype}
-**━━━━━━━━━━━━━━━**"""
+        logger_text = f"""━━━━━━━━━━━━━━━
+◍ [⌞ 𝘾𝙍 𖢻 ⌯ 𝙈𝙐𝙎𝙄𝘾 ⌯ ˹🎧˼⁩ ⌝
+━━━━━━━━━━━━━━━
+🌹️ اسم المجموعة : > {message.chat.title} [{message.chat.id}]
+━━━━━━━━━━━━━━━
+🥀 اسم المستخدم : › {message.from_user.mention}
+━━━━━━━━━━━━━━━
+🌸 يوزر المستخدم : › @{message.from_user.username}
+━━━━━━━━━━━━━━━
+🌷 ايدي المستخدم  : › {message.from_user.id}
+━━━━━━━━━━━━━━━
+🌿 رابط الجروب: > {chatusername}
+━━━━━━━━━━━━━━━
+🌻 المطلوب: {message.text}
+━━━━━━━━━━━━━━━
+💐 نوع التشغيل: {streamtype}
+━━━━━━━━━━━━━━━"""
         if message.chat.id != LOGGER_ID:
             try:
                 await app.send_message(
