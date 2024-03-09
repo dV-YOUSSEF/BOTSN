86c2766ceab4c73d458ae215e74354b70b3035cc
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-09 05:01:23 +0200

    Update همسه.py

--- ZeMusic/plugins/play/همسه.py
+++ ZeMusic/plugins/play/همسه.py
@@ -1,10 +1,11 @@
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from ZeMusic import app
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from DAXXMUSIC import app
 
 hmses = {}
+waiting_for_hms = False
+hms_ids = ""
 
-# هذه الدالة ترد على رسائل الهمسة برابط لإرسال الهمسة
 @app.on_message(filters.reply & filters.regex("همسه") & filters.group)
 async def reply_with_link(client, message):
     user_id = message.reply_to_message.from_user.id
@@ -18,14 +19,10 @@ async def reply_with_link(client, message):
     )
     await message.reply_text("\n╢ إضغط لإرسال همسه!\n", reply_markup=reply_markup)
 
-# هذه الدالة تبدأ عملية إرسال الهمسة
 @app.on_message(filters.command("start"))
 async def hms_start(client, message):
-    print("تم استدعاء دالة hms_start")  # طباعة رسالة للتحقق من استدعاء الدالة
-    print(f"نص الرسالة: {message.text}")  # طباعة نص الرسالة للتحقق من المحتوى
-    
+    global waiting_for_hms, hms_ids
     if message.text.split(" ", 1)[-1].startswith("hms"):
-        global waiting_for_hms, hms_ids
         hms_ids = message.text
         waiting_for_hms = True
         await message.reply_text(
@@ -35,35 +32,30 @@ async def hms_start(client, message):
             ),
         )
 
-# هذه الدالة تستقبل الهمسات وترسلها للمستخدم المعني
 @app.on_message(filters.private & filters.text & ~filters.command("start"))
 async def send_hms(client, message):
     global waiting_for_hms
-    if waiting_for_hms:
-        if "to" in hms_ids and "in" in hms_ids and "hms" in hms_ids:
-            to_id = int(hms_ids.split("to")[-1].split("in")[0])
-            from_id = int(hms_ids.split("hms")[-1].split("to")[0])
-            in_id = int(hms_ids.split("in")[-1])
-            to_url = f"tg://openmessage?user_id={to_id}"
-            from_url = f"tg://openmessage?user_id={from_id}"
-            
-            hmses[str(to_id)] = {"hms": message.text, "bar": in_id}
-            
-            await message.reply_text("-> تم ارسال الهمسه.\n√")
-            
-            await app.send_message(
-                chat_id=in_id,
-                text=f"╖ المستخدم [{(await app.get_chat(to_id)).first_name}]({to_url})\n╢ لديك همسه من البني آدم دا [{(await app.get_chat(from_id)).first_name}]({from_url})\n╜انت فقط من يستطيع رؤيتها 🔐",
-                reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("- اضغط لرؤية الهمسه 👀", callback_data="hms_answer")]]
-                ),
-            )
-            
-            waiting_for_hms = False
-        else:
-            await message.reply_text("حدث خطأ في تنسيق الهمسة، يرجى إعادة المحاولة.")
-
-# هذه الدالة تعرض الهمسة للمستخدم الذي تم ارسال الهمسة له
+    if waiting_for_hms:    
+        to_id = int(hms_ids.split("to")[-1].split("in")[0])
+        from_id = int(hms_ids.split("hms")[-1].split("to")[0])
+        in_id = int(hms_ids.split("in")[-1])
+        to_url = f"tg://openmessage?user_id={to_id}"
+        from_url = f"tg://openmessage?user_id={from_id}"
+        
+        hmses[str(to_id)] = {"hms": message.text, "bar": in_id}
+        
+        await message.reply_text("-> تم ارسال الهمسه.\n√")
+        
+        await app.send_message(
+            chat_id=in_id,
+            text=f"╖ المستخدم [{(await app.get_chat(to_id)).first_name}]({to_url})\n╢ لديك همسه من البني آدم دا [{(await app.get_chat(from_id)).first_name}]({from_url})\n╜انت فقط من يستطيع رؤيتها 🔐",
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("- اضغط لرؤية الهمسه 👀", callback_data="hms_answer")]]
+            ),
+        )
+        
+        waiting_for_hms = False
+     
 @app.on_callback_query(filters.regex("hms_answer"))
 async def display_hms(client, callback):
     in_id = callback.message.chat.id
@@ -74,8 +66,7 @@ async def display_hms(client, callback):
             await callback.answer(hmses.get(str(who_id))["hms"], show_alert=True)
     else:
         await callback.answer("بطل لعب ف حاجه مش بتاعتك يابابا 🗿", show_alert=True)
-
-# هذه الدالة تلغي عملية إرسال الهمسة
+        
 @app.on_callback_query(filters.regex("hms_cancel"))
 async def cancel_hms(client, callback):
     
