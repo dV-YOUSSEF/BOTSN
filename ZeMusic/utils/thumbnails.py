ef19eda41709ed427b112cfb01923f1f8b7a6c2e
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-10 09:48:49 +0200

    Update thumbnails.py

--- ZeMusic/utils/thumbnails.py
+++ ZeMusic/utils/thumbnails.py
@@ -1,88 +1,161 @@
 import os
+import re
 import aiofiles
 import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
+from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 from ZeMusic import app
 from config import YOUTUBE_IMG_URL
 
-def change_image_size(max_width, max_height, image):
-    width_ratio = max_width / image.size[0]
-    height_ratio = max_height / image.size[1]
-    new_width = int(width_ratio * image.size[0])
-    new_height = int(height_ratio * image.size[1])
-    new_image = image.resize((new_width, new_height))
-    return new_image
+def changeImageSize(maxWidth, maxHeight, image):
+    widthRatio = maxWidth / image.size[0]
+    heightRatio = maxHeight / image.size[1]
+    newWidth = int(widthRatio * image.size[0])
+    newHeight = int(heightRatio * image.size[1])
+    newImage = image.resize((newWidth, newHeight))
+    return newImage
 
-async def gen_bot(client, username, photo):
-    # Check if the image already exists
-    if os.path.isfile(f"{username}.png"):
-        return f"{username}.png"
+def truncate(text):
+    list = text.split(" ")
+    text1 = ""
+    text2 = ""    
+    for i in list:
+        if len(text1) + len(i) < 30:        
+            text1 += " " + i
+        elif len(text2) + len(i) < 30:       
+            text2 += " " + i
 
-    # Get served users and chats count
-    users = len(await get_served_users(client))
-    chats = len(await get_served_chats(client))
+    text1 = text1.strip()
+    text2 = text2.strip()     
+    return [text1,text2]
 
-    # Get thumbnail URL from YouTube video
-    url = "https://www.youtube.com/watch?v=gKA2XFkJZhI"
+def crop_center_circle(img, output_size, border, crop_scale=1.5):
+    half_the_width = img.size[0] / 2
+    half_the_height = img.size[1] / 2
+    larger_size = int(output_size * crop_scale)
+    img = img.crop(
+        (
+            half_the_width - larger_size/2,
+            half_the_height - larger_size/2,
+            half_the_width + larger_size/2,
+            half_the_height + larger_size/2
+        )
+    )
+    
+    img = img.resize((output_size - 2*border, output_size - 2*border))
+    
+    
+    final_img = Image.new("RGBA", (output_size, output_size), "white")
+    
+    
+    mask_main = Image.new("L", (output_size - 2*border, output_size - 2*border), 0)
+    draw_main = ImageDraw.Draw(mask_main)
+    draw_main.ellipse((0, 0, output_size - 2*border, output_size - 2*border), fill=255)
+    
+    final_img.paste(img, (border, border), mask_main)
+    
+    
+    mask_border = Image.new("L", (output_size, output_size), 0)
+    draw_border = ImageDraw.Draw(mask_border)
+    draw_border.ellipse((0, 0, output_size, output_size), fill=255)
+    
+    result = Image.composite(final_img, Image.new("RGBA", final_img.size, (0, 0, 0, 0)), mask_border)
+    
+    return result
+
+
+
+async def get_thumb(videoid):
+    if os.path.isfile(f"cache/{videoid}_v4.png"):
+        return f"cache/{videoid}_v4.png"
+
+    url = f"https://www.youtube.com/watch?v={videoid}"
     results = VideosSearch(url, limit=1)
     for result in (await results.next())["result"]:
+        try:
+            title = result["title"]
+            title = re.sub("\W+", " ", title)
+            title = title.title()
+        except:
+            title = "Unsupported Title"
+        try:
+            duration = result["duration"]
+        except:
+            duration = "Unknown Mins"
         thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        try:
+            views = result["viewCount"]["short"]
+        except:
+            views = "Unknown Views"
+        try:
+            channel = result["channel"]["name"]
+        except:
+            channel = "Unknown Channel"
 
-    # Download and save the thumbnail
     async with aiohttp.ClientSession() as session:
         async with session.get(thumbnail) as resp:
             if resp.status == 200:
-                async with aiofiles.open(f"thumb{username}.png", mode="wb") as f:
-                    await f.write(await resp.read())
+                f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
 
-    # Open images
-    youtube = Image.open(f"{photo}")
-    mostafa = Image.open(f"{photo}")
-
-    # Resize YouTube image
-    resized_youtube = change_image_size(1280, 720, youtube)
-    resized_youtube = resized_youtube.convert("RGBA")
-
-    # Apply blur effect to the resized image
-    background = resized_youtube.filter(filter=ImageFilter.BoxBlur(5))
+    youtube = Image.open(f"cache/thumb{videoid}.png")
+    image1 = changeImageSize(1280, 720, youtube)
+    image2 = image1.convert("RGBA")
+    background = image2.filter(filter=ImageFilter.BoxBlur(20))
     enhancer = ImageEnhance.Brightness(background)
     background = enhancer.enhance(0.6)
-
-    # Crop and resize mostafa image
-    x_center = mostafa.width / 2
-    y_center = mostafa.height / 2
-    x1 = x_center - 250
-    y1 = y_center - 250
-    x2 = x_center + 250
-    y2 = y_center + 250
-    logo = mostafa.crop((x1, y1, x2, y2))
-    logo.thumbnail((520, 520), Image.ANTIALIAS)
-    logo = ImageOps.expand(logo, border=15, fill="white")
-
-    # Paste logo onto background
-    background.paste(logo, (50, 100))
-
-    # Draw text on the image
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype("font2.ttf", 40)
-    font2 = ImageFont.truetype("font2.ttf", 70)
-    arial = ImageFont.truetype("font2.ttf", 30)
+    arial = ImageFont.truetype("ZeMusic/assets/font2.ttf", 30)
+    font = ImageFont.truetype("ZeMusic/assets/font.ttf", 30)
+    title_font = ImageFont.truetype("ZeMusic/assets/font.ttf", 45)
 
-    draw.text((600, 150), "Music Player BoT", fill="white", stroke_width=2, stroke_fill="white", font=font2)
-    draw.text((600, 340), "Dev : ALASUOTEY", fill="white", stroke_width=1, stroke_fill="white", font=font)
-    draw.text((600, 280), "Playing Music & Video", fill="white", stroke_width=1, stroke_fill="white", font=font)
-    draw.text((600, 400), f"user : {users}", (255, 255, 255), font=arial)
-    draw.text((600, 450), f"chats : {chats}", (255, 255, 255), font=arial)
-    draw.text((600, 500), "Version : 0.1.5", (255, 255, 255), font=arial)
-    draw.text((600, 550), f"BoT : t.me\{username}", (255, 255, 255), font=arial)
 
-    # Remove temporary thumbnail file
+    circle_thumbnail = crop_center_circle(youtube, 400, 20)
+    circle_thumbnail = circle_thumbnail.resize((400, 400))
+    circle_position = (120, 160)
+    background.paste(circle_thumbnail, circle_position, circle_thumbnail)
+
+    text_x_position = 565
+
+    title1 = truncate(title)
+    draw.text((text_x_position, 180), title1[0], fill=(255, 255, 255), font=title_font)
+    draw.text((text_x_position, 230), title1[1], fill=(255, 255, 255), font=title_font)
+    draw.text((text_x_position, 320), f"{channel}  |  {views[:23]}", (255, 255, 255), font=arial)
+
+    
+    line_length = 580  
+
+    
+    red_length = int(line_length * 0.6)
+    white_length = line_length - red_length
+
+    
+    start_point_red = (text_x_position, 380)
+    end_point_red = (text_x_position + red_length, 380)
+    draw.line([start_point_red, end_point_red], fill="red", width=9)
+
+    
+    start_point_white = (text_x_position + red_length, 380)
+    end_point_white = (text_x_position + line_length, 380)
+    draw.line([start_point_white, end_point_white], fill="white", width=8)
+
+    
+    circle_radius = 10 
+    circle_position = (end_point_red[0], end_point_red[1])
+    draw.ellipse([circle_position[0] - circle_radius, circle_position[1] - circle_radius,
+                  circle_position[0] + circle_radius, circle_position[1] + circle_radius], fill="red")
+    draw.text((text_x_position, 400), "00:00", (255, 255, 255), font=arial)
+    draw.text((1080, 400), duration, (255, 255, 255), font=arial)
+
+    play_icons = Image.open("ZeMusic/assets/play_icons.png")
+    play_icons = play_icons.resize((580, 62))
+    background.paste(play_icons, (text_x_position, 450), play_icons)
+
     try:
-        os.remove(f"thumb{username}.png")
+        os.remove(f"cache/thumb{videoid}.png")
     except:
         pass
-
-    # Save the final image
-    background.save(f"{username}.png")
-    return f"{username}.png"
+    background.save(f"cache/{videoid}_v4.png")
+    return f"cache/{videoid}_v4.png"
