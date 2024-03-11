b585e254878ef65a86f92b064bd21597a2571500
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-11 14:29:15 +0200

    Update تاك.py

--- ZeMusic/plugins/play/تاك.py
+++ ZeMusic/plugins/play/تاك.py
@@ -1,107 +1,67 @@
 import asyncio
-import os
-import time
-import requests
-import aiohttp
-from strings.filters import command
-from pyrogram import filters
-from pyrogram import Client
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
-from ZeMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from ZeMusic import app
-from asyncio import gather
-from pyrogram.errors import FloodWait
+from pyrogram import filters
+from strings.filters import command
 
-@app.on_message(command(["المالك", "صاحب الخرابه", "المنشي"]) & filters.group)
-async def gak_owne(client: Client, message: Message):
-      if len(message.command) >= 2:
-         return 
-      else:
-            chat_id = message.chat.id
-            f = "administrators"
-            async for member in client.iter_chat_members(chat_id, filter=f):
-               if member.status == "creator":
-                 id = member.user.id
-                 key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
-                 m = await client.get_chat(id)
-                 if m.photo:
-                       photo = await app.download_media(m.photo.big_file_id)
-                       return await message.reply_photo(photo, caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🎃 ¦𝙸𝙳 :`{m.id}`\n💌 ¦𝙱𝙸𝙾 :{m.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
-                 else:
-                    return await message.reply("• " + member.user.mention)
-                    
-                    
-   
+SPAM_CHATS = []
 
-   
-@app.on_message(command(["اسمي", "اسمي اي"]) & filters.group )
-async def vgdg(client: Client, message: Message):
-    await message.reply_text(
-        f"""❤️‍🔥 اسمك »»  {message.from_user.mention()}""") 
 
-        
-
-array = []
-@app.on_message(command([""]) & ~filters.private)
-async def nummmm(client: app, message):
-  if message.chat.id in array:
-     return await message.reply_text("**التاك قيد التشغيل حالياً ،**")
-  chek = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if not chek.status in ["administrator", "creator"]:
-    await message.reply("**يجب انت تكون مشرف لاستخدام الامر 🖱️**")
-    return
-  await message.reply_text("**جاري بدأ المنشن ، لايقاف الامر اضغط **\n /cancel او اكتب بس منشن")
-  i = 0
-  txt = ""
-  zz = message.text
-  if message.photo:
-          photo_id = message.photo.file_id
-          photo = await client.download_media(photo_id)
-          zz = message.caption
-  try:
-   zz = zz.replace("@all","").replace("تاك","").replace("نادي الكل","")
-  except:
-    pass
-  array.append(message.chat.id)
-  async for x in client.iter_chat_members(message.chat.id):
-      if message.chat.id not in array:
+@app.on_message(command(["/mantion", "تاك للكل", "تاك", "@all"]) & filters.group)
+async def tag_all_users(_, message):
+    replied = message.reply_to_message
+    if len(message.command) < 2 and not replied:
+        await message.reply_text("**رد على الرسالة أو اكتب شيئًا أو استخدم أمرًا!**")
         return
-      if not x.user.is_deleted:
-       i += 1
-       txt += f" {x.user.mention} ،"
-       if i == 5:
+    if replied:
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 5
+            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            if usernum == 1:
+                await replied.reply_text(usertxt)
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
         try:
-              if not message.photo:
-                    await client.send_message(message.chat.id, f"{zz}\n{txt}")
-              else:
-                    await client.send_photo(message.chat.id, photo=photo, caption=f"{zz}\n{txt}")
-              i = 0
-              txt = ""
-              await asyncio.sleep(2)
-        except FloodWait as e:
-                    flood_time = int(e.x)
-                    if flood_time > 200:
-                        continue
-                    await asyncio.sleep(flood_time)
+            SPAM_CHATS.remove(message.chat.id)
         except Exception:
-              array.remove(message.chat.id)
-  array.remove(message.chat.id)
+            pass
+    else:
+        text = message.text.split(None, 1)[1]
+
+        SPAM_CHATS.append(message.chat.id)
+        usernum = 0
+        usertxt = ""
+        async for m in app.get_chat_members(message.chat.id):
+            if message.chat.id not in SPAM_CHATS:
+                break
+            usernum += 1
+            usertxt += f"\n⊚ [{m.user.first_name}](tg://user?id={m.user.id})\n"
+            if usernum == 5:
+                await app.send_message(message.chat.id, f'{text}\n{usertxt}')
+                await asyncio.sleep(2)
+                usernum = 0
+                usertxt = ""
+        try:
+            SPAM_CHATS.remove(message.chat.id)
+        except Exception:
+            pass
 
 
-@app.on_message(command(["بس المنشن", "/cancel","بس منشن"]))
-async def stop(client, message):
-  chek = await client.get_chat_member(message.chat.id, message.from_user.id)
-  if not chek.status in ["administrator", "creator"]:
-    await message.reply("**يجب انت تكون مشرف لاستخدام الامر 🖱️")
-    return
-  if message.chat.id not in array:
-     await message.reply("**المنشن متوقف بالفعل**")
-     return 
-  if message.chat.id in array:
-    array.remove(message.chat.id)
-    await message.reply("**تم ايقاف المنشن بنجاح✅**")
-    return
+@app.on_message(command(["/cancel", "ايقاف التاك"]) & ~filters.private)
+async def cancelcmd(_, message):
+    chat_id = message.chat.id
+    if chat_id in SPAM_CHATS:
+        try:
+            SPAM_CHATS.remove(chat_id)
+        except Exception:
+            pass
+        return await message.reply_text("**اعتذر، هل يمكنك توضيح ذلك بشكل أكثر دقة؟**")
 
-
-
-
+    else:
+        await message.reply_text("**لا يوجد عملية جارية!**")
+        return       
