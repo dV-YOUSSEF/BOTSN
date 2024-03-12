27d45a839e9bda5618f5bfbf9e133fe569ae99a6
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-12 05:31:29 +0200

    Update الاوامر2.py

--- ZeMusic/plugins/play/الاوامر2.py
+++ ZeMusic/plugins/play/الاوامر2.py
@@ -9,7 +9,6 @@ from pyrogram.enums import *
 from strings.filters import command
 #BOT FILE NAME
 from ZeMusic import app as app
-from ZeMusic.mongo.couples_db import _get_image, get_couple
 
 def dt():
     now = datetime.now()
