071e1c9851178a14561de885aa2fd49c383bb900
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-12 07:28:35 +0200

    Update play.py

--- ZeMusic/plugins/play/play.py
+++ ZeMusic/plugins/play/play.py
@@ -4,7 +4,7 @@ import string
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
-
+from strings.filters import command
 import config
 from ZeMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
 from ZeMusic.core.call import Mody
@@ -26,21 +26,33 @@ from config import BANNED_USERS, lyrical
 
 
 @app.on_message(
-    filters.command(
+     command(
         [
+            "play",
+            "vplay",
+            "cplay",
+            "cvplay",
+            "playforce",
+            "vplayforce",
+            "cplayforce",
+            "cvplayforce",
             "/play",
-            "تشغيل",
-            "شغل",
+            "/vplay", 
+            "cplay", 
+            "g", 
+            "تشغيل", 
+            "فديو",
+            "پ کەناڵ",
             "",
-            "/vplay",
-            "/cplay",
-            "/cvplay",
-            "/playforce",
-            "/vplayforce",
-            "/cplayforce",
-            "/cvplayforce",
-        ],""
+            "فيديو",
+            "پ ئەلینا",
+            "شغل",
+            "سورەتی",
+            "سورەت",
+            "سوڕەت",
+        ]
     )
+    & ~filters.private
     & ~BANNED_USERS
 )
 @PlayWrapper
@@ -62,7 +74,7 @@ async def play_commnd(
     slider = None
     plist_type = None
     spotify = None
-    user_id = message.from_user.id if message.from_user else "1121532100"
+    user_id = message.from_user.id if message.from_user else "033365581"
     user_name = message.from_user.first_name if message.from_user else "None"
     audio_telegram = (
         (message.reply_to_message.audio or message.reply_to_message.voice)
@@ -290,7 +302,7 @@ async def play_commnd(
             return await mystic.delete()
         else:
             try:
-                await mody.stream_call(url)
+                await DAXX.stream_call(url)
             except NoActiveGroupCall:
                 await mystic.edit_text(_["black_9"])
                 return await app.send_message(
@@ -503,8 +515,8 @@ async def play_music(client, CallbackQuery, _):
     return await mystic.delete()
 
 
-@app.on_callback_query(filters.regex("modymousAdmin") & ~BANNED_USERS)
-async def modymous_check(client, CallbackQuery):
+@app.on_callback_query(filters.regex("DAXXmousAdmin") & ~BANNED_USERS)
+async def DAXXmous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
             "» ʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ :\n\nᴏᴘᴇɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ sᴇᴛᴛɪɴɢs.\n-> ᴀᴅᴍɪɴɪsᴛʀᴀᴛᴏʀs\n-> ᴄʟɪᴄᴋ ᴏɴ ʏᴏᴜʀ ɴᴀᴍᴇ\n-> ᴜɴᴄʜᴇᴄᴋ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ ᴘᴇʀᴍɪssɪᴏɴs.",
@@ -514,7 +526,7 @@ async def modymous_check(client, CallbackQuery):
         pass
 
 
-@app.on_callback_query(filters.regex("modyPlaylists") & ~BANNED_USERS)
+@app.on_callback_query(filters.regex("DAXXPlaylists") & ~BANNED_USERS)
 @languageCB
 async def play_playlists_command(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
