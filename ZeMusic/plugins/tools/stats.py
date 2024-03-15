231a727e721928dbd86f5d796a037f32324f156d
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-15 18:34:46 +0200

    Update stats.py

--- ZeMusic/plugins/tools/stats.py
+++ ZeMusic/plugins/tools/stats.py
@@ -3,7 +3,6 @@ from sys import version as pyver
 
 import psutil
 from pyrogram import __version__ as pyrover
-from ZeMusic.plugins.play.filters import command
 from pyrogram import filters
 from pyrogram.errors import MessageIdInvalid
 from pyrogram.types import InputMediaPhoto, Message
@@ -20,36 +19,117 @@ from ZeMusic.utils.inline.stats import back_stats_buttons, stats_buttons
 from config import BANNED_USERS
 
 
-# Function to get statistics
-def get_statistics():
-    with app:
-        all_chats = app.get_dialogs()
-        group_count = 0
-        user_count = 0
-        group_links = []
-        user_links = []
+@app.on_message(filters.command(["stats", "gstats"]) & filters.group & ~BANNED_USERS)
+@language
+async def stats_global(client, message: Message, _):
+    upl = stats_buttons(_, True if message.from_user.id in SUDOERS else False)
+    await message.reply_photo(
+        photo=config.STATS_IMG_URL,
+        caption=_["gstats_2"].format(app.mention),
+        reply_markup=upl,
+    )
 
-        for chat in all_chats:
-            if chat.chat.type == "group" or chat.chat.type == "supergroup":
-                group_count += 1
-                group_links.append(f"{chat.chat.title}: t.me/{chat.chat.username}")
-            elif chat.chat.type == "private":
-                user_count += 1
-                user_links.append(f"{chat.chat.first_name}: t.me/{chat.chat.username}")
 
-        statistics = f"Total groups: {group_count}\nTotal users: {user_count}\n\nGroup links:\n"
-        for link in group_links:
-            statistics += link + "\n"
-        statistics += "\nUser links:\n"
-        for link in user_links:
-            statistics += link + "\n"
+@app.on_callback_query(filters.regex("stats_back") & ~BANNED_USERS)
+@languageCB
+async def home_stats(client, CallbackQuery, _):
+    upl = stats_buttons(_, True if CallbackQuery.from_user.id in SUDOERS else False)
+    await CallbackQuery.edit_message_text(
+        text=_["gstats_2"].format(app.mention),
+        reply_markup=upl,
+    )
 
-        return statistics
 
-# Command to fetch statistics
-@app.on_message(filters.command("statistics", prefixes="/") & filters.me)
-def fetch_statistics(_, message: Message):
-    statistics = get_statistics()
-    message.reply(statistics)
+@app.on_callback_query(filters.regex("TopOverall") & ~BANNED_USERS)
+@languageCB
+async def overall_stats(client, CallbackQuery, _):
+    await CallbackQuery.answer()
+    upl = back_stats_buttons(_)
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
+    await CallbackQuery.edit_message_text(_["gstats_1"].format(app.mention))
+    served_chats = len(await get_served_chats())
+    served_users = len(await get_served_users())
+    text = _["gstats_3"].format(
+        app.mention,
+        len(assistants),
+        len(BANNED_USERS),
+        served_chats,
+        served_users,
+        len(ALL_MODULES),
+        len(SUDOERS),
+        config.AUTO_LEAVING_ASSISTANT,
+        config.DURATION_LIMIT_MIN,
+    )
+    med = InputMediaPhoto(media=config.STATS_IMG_URL, caption=text)
+    try:
+        await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
+    except MessageIdInvalid:
+        await CallbackQuery.message.reply_photo(
+            photo=config.STATS_IMG_URL, caption=text, reply_markup=upl
+        )
 
-app.run()
+
+@app.on_callback_query(filters.regex("bot_stats_sudo"))
+@languageCB
+async def bot_stats(client, CallbackQuery, _):
+    if CallbackQuery.from_user.id not in SUDOERS:
+        return await CallbackQuery.answer(_["gstats_4"], show_alert=True)
+    upl = back_stats_buttons(_)
+    try:
+        await CallbackQuery.answer()
+    except:
+        pass
+    await CallbackQuery.edit_message_text(_["gstats_1"].format(app.mention))
+    p_core = psutil.cpu_count(logical=False)
+    t_core = psutil.cpu_count(logical=True)
+    ram = str(round(psutil.virtual_memory().total / (1024.0**3))) + " ɢʙ"
+    try:
+        cpu_freq = psutil.cpu_freq().current
+        if cpu_freq >= 1000:
+            cpu_freq = f"{round(cpu_freq / 1000, 2)}ɢʜᴢ"
+        else:
+            cpu_freq = f"{round(cpu_freq, 2)}ᴍʜᴢ"
+    except:
+        cpu_freq = "ғᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ"
+    hdd = psutil.disk_usage("/")
+    total = hdd.total / (1024.0**3)
+    used = hdd.used / (1024.0**3)
+    free = hdd.free / (1024.0**3)
+    call = await mongodb.command("dbstats")
+    datasize = call["dataSize"] / 1024
+    storage = call["storageSize"] / 1024
+    served_chats = len(await get_served_chats())
+    served_users = len(await get_served_users())
+    text = _["gstats_5"].format(
+        app.mention,
+        len(ALL_MODULES),
+        platform.system(),
+        ram,
+        p_core,
+        t_core,
+        cpu_freq,
+        pyver.split()[0],
+        pyrover,
+        pytgver,
+        str(total)[:4],
+        str(used)[:4],
+        str(free)[:4],
+        served_chats,
+        served_users,
+        len(BANNED_USERS),
+        len(await get_sudoers()),
+        str(datasize)[:6],
+        storage,
+        call["collections"],
+        call["objects"],
+    )
+    med = InputMediaPhoto(media=config.STATS_IMG_URL, caption=text)
+    try:
+        await CallbackQuery.edit_message_media(media=med, reply_markup=upl)
+    except MessageIdInvalid:
+        await CallbackQuery.message.reply_photo(
+            photo=config.STATS_IMG_URL, caption=text, reply_markup=upl
+        )
