0587330688521d415e1bb6b2f2d450151c9dfbbc
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-20 00:41:58 +0200

    Update thumbnails.py

--- ZeMusic/utils/thumbnails.py
+++ ZeMusic/utils/thumbnails.py
@@ -17,8 +17,6 @@ from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 
-from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
-
 
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
