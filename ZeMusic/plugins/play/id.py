a0b7c78cbd7e64ea66650e9e2ff93c4eae497808
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-19 17:26:04 +0200

    Update id.py

--- ZeMusic/plugins/play/id.py
+++ ZeMusic/plugins/play/id.py
@@ -17,73 +17,126 @@ from time import time
 from os import getenv
 from dotenv import load_dotenv
 from pyrogram import filters
-from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from pyrogram.types import ChatMemberStatus
-from pyrogram.types import CallbackQuery
 
-iddof = []
 
-@app.on_message(filters.command(["تعطيل الايدي", "قفل الايدي"], "") & filters.group)
-async def iddlock(_, message):
-    get = await app.get_chat_member(message.chat.id, message.from_user.id)
-    if get.status in ["creator", "administrator"]:
-        if message.chat.id in iddof:
-            return await message.reply_text("**♪ الامر معطل من قبل  💎 .**")
-        iddof.append(message.chat.id)
-        return await message.reply_text("**♪ تم تعطيل الايدي بنجاح  💎 .**")
-    else:
-        return await message.reply_text("**♪ عذرا عزيزي هذا الامر للادمن الجروب فقط  💎 .**")
 
-@app.on_message(filters.command(["فتح الايدي", "تفعيل الايدي"], "") & filters.group)
-async def iddopen(_, message):
-    get = await app.get_chat_member(message.chat.id, message.from_user.id)
-    if get.status in ["creator", "administrator"]:
-        if not message.chat.id in iddof:
-            return await message.reply_text("**♪ الايدي مفعل من قبل  💎 .**")
-        iddof.remove(message.chat.id)
-        return await message.reply_text("**♪ تم تفعيل الايدي بنجاح  💎 .**")
-    else:
-        return await message.reply_text("**♪ عذرا عزيزي هذا الامر للادمن الجروب فقط  💎 .**")
 
-@app.on_message(filters.command(["ايدي"], ""))
-async def muid(client, message):
-    if message.chat.id in iddof:
-        return await message.reply_text("**♪ تم تعطيل امر الايدي من قبل المشرفين  💎 .**")
-    user = await app.get_chat(message.from_user.id)
-    user_id = user.id
-    username = user.username
-    first_name = user.first_name
-    bioo = user.bio
-    photo = user.photo.big_file_id
-    photo = await app.download_media(photo)
-    idd = len(id.get(user_id, []))
-    await message.reply_photo(
-        photo=photo,
-        caption=f"**name : {first_name}\nid : {user_id}\nuser : [@{username}]\nbio : {bioo}**",
+load_dotenv()
+
+
+
+
+def get_file_id(msg: Message):
+
+    if msg.media:
+
+        for message_type in (
+
+            "photo",
+
+            "animation",
+
+            "audio",
+
+            "document",
+
+            "video",
+
+            "video_note",
+
+            "voice",
+
+            # "contact",
+
+            # "dice",
+
+            # "poll",
+
+            # "location",
+
+            # "venue",
+
+            "sticker",
+
+        ):
+
+            obj = getattr(msg, message_type)
+
+            if obj:
+
+                setattr(obj, "message_type", message_type)
+
+                return obj
+
+
+
+
+
+
+
+#@app.on_message(command(["ا"]) & filters.group &
+#async def khalid(client: Client, message: Message):
+    #usr = await client.get_users(message.from_user.id)
+    #name = usr.first_name
+    #async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+                    #await message.reply_photo(photo.file_id,       caption=f"""ᴜsᴇʀ -› {message.from_user.mention}\n𝘂𝘀𝗲𝗿𝗻𝗮𝗺𝗲 -› @{message.from_user.username}\nɪᴅ -› {message.from_user.id}\nbio » {bio}""", 
+        #reply_markup=InlineKeyboardMarkup(
+
+            #[
+
+                #[
+
+                    #InlineKeyboardButton(
+
+                        #name, url=f"https://t.me/{message.from_user.id}")
+
+                #],
+
+            #]
+
+        #),
+
+    #)
+
+@app.on_message(filters.regex("^ا$") & filters.group)
+async def khalid(client: Client, message: Message):
+
+    usr = await client.get_chat(message.from_user.id)
+
+    name = usr.first_name
+
+    bio = usr.bio
+
+
+
+
+    async for photo in client.iter_profile_photos(message.from_user.id, limit=1):
+
+                    await message.reply_photo(photo.file_id,       caption=f"""**↯ : وفي النهاية أنتم السيئون وهم الأبرياء**
+            
+**↯ : اسمك : › {message.from_user.mention}**
+                    
+**↯ : معرفك : › @{message.from_user.username}**
+                    
+**↯ : ايديك : › `{message.from_user.id}`**
+                    
+**↯ : البايو : › \n {bio}**""", 
+
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(f"{idd} 🤍", callback_data=f"heart{user_id}")],]
+
+            [
+
+                [
+
+                    InlineKeyboardButton(
+
+                                            name, user_id=6581896306)
+                ],
+
+            ]
+
         ),
+
     )
-
-id = {}
-
-@app.on_callback_query(filters.regex("heart"))
-async def heart(_, query: CallbackQuery):
-    callback_data = query.data.strip()
-    callback_request = callback_data.replace("heart", "")
-    user_id = int(callback_request)
-    user = await app.get_chat(user_id)
-    if not query.from_user.mention in id.get(user_id, []):
-        id[user_id] = id.get(user_id, []) + [query.from_user.mention]
-    else:
-        id[user_id].remove(query.from_user.mention)
-    idd = len(id.get(user_id, []))
-    await query.edit_message_text(
-        f"**name : {user.first_name}\nid : {user_id}\nuser : [@{user.username}]\nbio : {user.bio}**",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(f"{idd} 🤍", callback_data=f"heart{user_id}")]]
-        ),
-    )
-
-app.run()
+    
+    
