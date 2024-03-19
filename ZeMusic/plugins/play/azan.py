f63141d51d826391cc3b13e990f2089421606a1d
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-19 20:42:54 +0200

    Update azan.py

--- ZeMusic/plugins/play/azan.py
+++ ZeMusic/plugins/play/azan.py
@@ -11,7 +11,7 @@ from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
 from pytgcalls.exceptions import NoActiveGroupCall, TelegramServerError, AlreadyJoinedError
 from pyrogram.errors import ChatAdminRequired, UserAlreadyParticipant, UserNotParticipant
 
-from source.Data import get_call, get_userbot
+from zeMusic.Data import get_call, get_userbot
 
 
 cairo_timezone = pytz.timezone('Africa/Cairo')
