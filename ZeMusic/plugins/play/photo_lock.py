97a7e49e2cb5fdc0481d3b36598428d31617e93d
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-21 12:37:17 +0200

    Update photo_lock.py

--- ZeMusic/plugins/play/photo_lock.py
+++ ZeMusic/plugins/play/photo_lock.py
@@ -11,26 +11,58 @@ from pyrogram.types import Message
 lokrf = []
 
 @app.on_message(
-     filters.command(["قفل الصور", "تعطيل الصور"])
-     & filters.group
+    filters.command(["قفل الصور", "تعطيل الصور"])
+    & filters.group
 )
 async def close_photos(client: Client, message: Message):
-    if message.chat.id in lokrf:
-        return await message.reply_text(f"**الصور مقفلة بالفعل في هذه المجموعة**")
-    
-    lokrf.append(message.chat.id)
-    return await message.reply_text(f"**تم قفل إرسال الصور بنجاح في هذه المجموعة**")
+    if message.from_user.is_admin:
+        if message.chat.id in lokrf:
+            return await message.reply_text(f"**الصور مقفلة بالفعل في هذه المجموعة**")
+        
+        lokrf.append(message.chat.id)
+        await message.reply_text(f"**تم قفل إرسال الصور بنجاح في هذه المجموعة**")
+        
+        # قفل إرسال الصور في المجموعة
+        await client.set_chat_permissions(
+            message.chat.id,
+            can_send_messages=True,
+            can_send_media_messages=False,
+            can_send_polls=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True,
+            can_change_info=True,
+            can_invite_users=True,
+            can_pin_messages=True,
+        )
+    else:
+        await message.reply_text("**عذراً، يجب أن تكون مشرفاً لتنفيذ هذا الأمر**")
 
 @app.on_message(
     filters.command(["فتح الصور", "تفعيل الصور"])
     & filters.group
 )
 async def open_photos(client: Client, message: Message):
-    if message.chat.id not in lokrf:
-        return await message.reply_text(f"**إرسال الصور مفعل بالفعل في هذه المجموعة**")
-    
-    lokrf.remove(message.chat.id)
-    return await message.reply_text(f"**تم فتح إرسال الصور بنجاح في هذه المجموعة**")
+    if message.from_user.is_admin:
+        if message.chat.id not in lokrf:
+            return await message.reply_text(f"**إرسال الصور مفعل بالفعل في هذه المجموعة**")
+        
+        lokrf.remove(message.chat.id)
+        await message.reply_text(f"**تم فتح إرسال الصور بنجاح في هذه المجموعة**")
+        
+        # إعادة فتح إرسال الصور في المجموعة
+        await client.set_chat_permissions(
+            message.chat.id,
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_send_polls=True,
+            can_send_other_messages=True,
+            can_add_web_page_previews=True,
+            can_change_info=True,
+            can_invite_users=True,
+            can_pin_messages=True,
+        )
+    else:
+        await message.reply_text("**عذراً، يجب أن تكون مشرفاً لتنفيذ هذا الأمر**")
 
 if __name__ == "__main__":
     app.run()
