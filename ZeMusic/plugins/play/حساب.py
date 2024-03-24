251b3e9d866abb8b158c11419974869512638436
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-24 21:13:48 +0200

    Update حساب.py

--- ZeMusic/plugins/play/حساب.py
+++ ZeMusic/plugins/play/حساب.py
@@ -1,37 +1,130 @@
-import asyncio
-import pyrogram
-import random
-import datetime
-from ZeMusic import app
-from pyrogram import Client
-from pyrogram import filters
-from pyrogram.types import Message
 from pyrogram import Client, filters
-from pyrogram.types import ChatMemberUpdated, InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.types import (InlineKeyboardButton,
-
-                            InlineKeyboardMarkup, Message)
-
-
-
+from pyrogram import enums
+from pyrogram.enums import ChatMembersFilter, ChatMemberStatus , ChatType
+from pyrogram.types import ChatPermissions, ChatPrivileges
+from ZeMusic import app
+import asyncio
 
 
 
-@app.on_message(filters.regex(r'حساب العمر'))
-async def calculate_age(client, message):
-    try:
-        birth_date = datetime.datetime.strptime(message.text.split(" ")[2], "%d-%m-%Y")
-    except:
-        await message.reply_text("خطأ عزيزي برجاء ادخال تاريخ ميلادك بهذه الصيغه مثال - ( 1994-1-1 )")
+
+welcome_enabled = True
+
+
+
+
+
+
+@app.on_chat_member_updated()
+async def welcome(client, chat_member_updated):
+    if not welcome_enabled:
         return
+    
+    if chat_member_updated.new_chat_member.status == ChatMemberStatus.BANNED:
+        kicked_by = chat_member_updated.new_chat_member.restricted_by
+        user = chat_member_updated.new_chat_member.user
+        
+        if kicked_by is not None and kicked_by.is_self:
+            messagee = f"• المستخدم {user.username} ({user.first_name}) تم طرده من الدردشة بواسطة البوت"
+        else:
+            if kicked_by is not None:
+                message = f"• المستخدم [{user.first_name}](tg://user?id={user.id}) \n• تم طرده من الدردشة بواسطة [{kicked_by.first_name}](tg://user?id={kicked_by.id})\n• ولقد طردته بسبب هذا"
+                try:
+                    await client.ban_chat_member(chat_member_updated.chat.id, kicked_by.id)
+                except Exception as e:
+                    message += f"\n\nعذرًا، لم استطع حظر الإداري بسبب: {str(e)}"
+            else:
+                message = f"• المستخدم {user.username} ({user.first_name}) تم طرده من الدردشة"
+            
+            
+        
+        await client.send_message(chat_member_updated.chat.id, message)
 
-    today = datetime.datetime.today()
-    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
-    months = (today.year - birth_date.year) * 12 + today.month - birth_date.month
-    days = (today - birth_date).days
-    next_birthday = datetime.datetime(today.year, birth_date.month, birth_date.day)
-    if today > next_birthday:
-        next_birthday = datetime.datetime(today.year+1, birth_date.month, birth_date.day)
-    remaining_days = (next_birthday - today).days
 
-    await message.reply_text(f" • عمرك هو >> {age} سنه\n\n  •عمرك ب الاشهر >> {months} شهر \n\n  •عمرك ب الايام >> {days} يوم.\n\n  •عيد ميلادك بعد >> {remaining_days} يوم.")
+
+
+@app.on_message(filters.command(["رفع مشرف"], "") & filters.channel)
+def promote_c_admin(client, message):
+    if message.reply_to_message and message.reply_to_message.from_user:
+        target = message.reply_to_message.from_user.id
+        user_id = str(target)
+    elif message.reply_to_message is None:
+        target = message.text.split()[2]
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("لا يمكن العثور على المستخدم")
+            return
+    else:
+        target = message.text.split()[1].strip("@")
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("لا يمكن العثور على المستخدم")
+            return
+
+    
+    ToM= ChatPrivileges(
+                    can_manage_chat=True,
+                    can_delete_messages=True,
+                    can_manage_video_chats=True,
+                    can_restrict_members=True,
+                    can_promote_members=False,
+                    can_change_info=False,
+                    can_post_messages=True,
+                    can_edit_messages=True,
+                    can_invite_users=True,
+                    can_pin_messages=False,
+                    is_anonymous=False
+                )
+    chat_id = message.chat.id
+    client.promote_chat_member(chat_id, user_id, ToM)
+    message.reply(f"تم رفع {user_id} ادمن بنجاح")
+    
+
+
+
+@app.on_message(filters.command(["رفع مشرف"], "") & filters.group)
+def promote_g_admin(client, message):
+    if message.reply_to_message and message.reply_to_message.from_user:
+        target = message.reply_to_message.from_user.id
+        user_id = str(target)
+    elif message.reply_to_message is None:
+        target = message.text.split()[2]
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("لا يمكن العثور على المستخدم")
+            return
+    else:
+        target = message.text.split()[1].strip("@")
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("لا يمكن العثور على المستخدم")
+            return
+
+    tom_id = message.from_user.id
+    chat_id = message.chat.id
+    ToM= ChatPrivileges(
+                    can_manage_chat=True,
+                    can_delete_messages=True,
+                    can_manage_video_chats=True,
+                    can_restrict_members=True,
+                    can_promote_members=True,
+                    can_change_info=True,
+                    can_post_messages=False,
+                    can_edit_messages=False,
+                    can_invite_users=True,
+                    can_pin_messages=True,
+                    is_anonymous=False
+                )
+    tooom = client.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS)
+    for tom in tooom:
+    	if tom.user.id == tom_id and (tom.status == enums.ChatMemberStatus.OWNER or tom.status == enums.ChatMemberStatus.ADMINISTRATOR):
+    		client.promote_chat_member(chat_id, user_id, ToM)
+    		message.reply(f"تم رفع {user_id} ادمن بنجاح")
