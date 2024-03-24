b41f37ba7a71d2eb79f711b9308d6a0603643345
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-25 00:08:42 +0200

    Update منع تصفيه.py

--- ZeMusic/plugins/play/منع تصفيه.py
+++ ZeMusic/plugins/play/منع تصفيه.py
@@ -42,9 +42,8 @@ async def welcome(client, chat_member_updated):
 
 
 
-
-@app.on_message(filters.command(["رفع مشرف"], "") & filters.channel)
-def promote_c_admin(client, message):
+@app.on_message(filters.command(["رفع مشرف"], "") & filters.group)
+def promote_g_admin(client, message):
     if message.reply_to_message and message.reply_to_message.from_user:
         target = message.reply_to_message.from_user.id
         user_id = str(target)
@@ -65,25 +64,23 @@ def promote_c_admin(client, message):
             message.reply_text("لا يمكن العثور على المستخدم")
             return
 
-    
+    tom_id = message.from_user.id
+    chat_id = message.chat.id
     ToM= ChatPrivileges(
                     can_manage_chat=True,
                     can_delete_messages=True,
                     can_manage_video_chats=True,
                     can_restrict_members=True,
-                    can_promote_members=False,
-                    can_change_info=False,
-                    can_post_messages=True,
-                    can_edit_messages=True,
+                    can_promote_members=True,
+                    can_change_info=True,
+                    can_post_messages=False,
+                    can_edit_messages=False,
                     can_invite_users=True,
-                    can_pin_messages=False,
+                    can_pin_messages=True,
                     is_anonymous=False
                 )
-    chat_id = message.chat.id
-    client.promote_chat_member(chat_id, user_id, ToM)
-    message.reply(f"تم رفع {user_id} ادمن بنجاح")
-    
-
-
-
-
+    tooom = client.get_chat_members(chat_id, filter=ChatMembersFilter.ADMINISTRATORS)
+    for tom in tooom:
+    	if tom.user.id == tom_id and (tom.status == enums.ChatMemberStatus.OWNER or tom.status == enums.ChatMemberStatus.ADMINISTRATOR):
+    		client.promote_chat_member(chat_id, user_id, ToM)
+    		message.reply(f"تم رفع {user_id} ادمن بنجاح")
