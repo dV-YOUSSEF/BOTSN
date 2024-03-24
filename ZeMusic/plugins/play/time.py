f8d9e2aebf3f65d527cddfc7cefe32e61925c5cc
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-24 18:47:53 +0200

    Update time.py

--- ZeMusic/plugins/play/time.py
+++ ZeMusic/plugins/play/time.py
@@ -1,15 +1,22 @@
+# استيراد الوحدات اللازمة
 from pyrogram import Client, filters
 from datetime import datetime
 import pytz
 from strings.filters import command
 from ZeMusic import app
 
+# دالة للحصول على الوقت الحالي بتوقيت مصر
 def get_current_time():
-    tz = pytz.timezone('Africa/Cairo')  # تعيين التوقيت لأفريقيا/القاهرة
+    tz = pytz.timezone('Africa/Cairo')
     current_time = datetime.now(tz)
     return current_time.strftime("<b>%p: %I:%M:%S</b>\n\n<b>%Y-%m-%d</b>\n\n<b>%Z%z</b>")
 
+# تعريف دالة التفاعل مع الأمر المرسل
 @app.on_message(command(["/Time","الوقت","وقت"]))
-def send_time(client, message):
+async def send_time(client, message):
+    # الحصول على الوقت الحالي
     time = get_current_time()
-    client.send_message(message.chat.id, f"<b>≪━━━━𝘴ꪮꪊ𝘳ᥴꫀ 𝘴ꪀipꫀ𝘳━━━━≫</b>\n\n{time}")
+    # إرسال الرسالة بالوقت المحدد إلى المحادثة
+    await client.send_message(message.chat.id, f"<b>≪━━━━𝘴ꪮꪊ𝘳ᥴꫀ 𝘴ꪀipꫀ𝘳━━━━≫\n\n{time}") 
+    # إرسال نص مع اقتباس إلى الرسالة الأصلية
+    await message.reply_text("source...", quote=True)
