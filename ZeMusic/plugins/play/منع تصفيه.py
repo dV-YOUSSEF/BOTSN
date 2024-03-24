c2f62597008b4db514df01ce2275eefa77cf069a
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-25 00:10:22 +0200

    Update منع تصفيه.py

--- ZeMusic/plugins/play/منع تصفيه.py
+++ ZeMusic/plugins/play/منع تصفيه.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram import enums
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus , ChatType
 from pyrogram.types import ChatPermissions, ChatPrivileges
-from ZeMusic import app
+from AnonXMusic import app
 import asyncio
 
 
@@ -39,6 +39,50 @@ async def welcome(client, chat_member_updated):
             
         
         await client.send_message(chat_member_updated.chat.id, message)
+
+
+
+
+@app.on_message(filters.command(["رفع مشرف"], "") & filters.channel)
+def promote_c_admin(client, message):
+    if message.reply_to_message and message.reply_to_message.from_user:
+        target = message.reply_to_message.from_user.id
+        user_id = str(target)
+    elif message.reply_to_message is None:
+        target = message.text.split()[2]
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("لا يمكن العثور على المستخدم")
+            return
+    else:
+        target = message.text.split()[1].strip("@")
+        user = app.get_users(target)
+        if user:
+            user_id = str(user.id)
+        else:
+            message.reply_text("لا يمكن العثور على المستخدم")
+            return
+
+    
+    ToM= ChatPrivileges(
+                    can_manage_chat=True,
+                    can_delete_messages=True,
+                    can_manage_video_chats=True,
+                    can_restrict_members=True,
+                    can_promote_members=False,
+                    can_change_info=False,
+                    can_post_messages=True,
+                    can_edit_messages=True,
+                    can_invite_users=True,
+                    can_pin_messages=False,
+                    is_anonymous=False
+                )
+    chat_id = message.chat.id
+    client.promote_chat_member(chat_id, user_id, ToM)
+    message.reply(f"تم رفع {user_id} ادمن بنجاح")
+    
 
 
 
@@ -83,4 +127,4 @@ def promote_g_admin(client, message):
     for tom in tooom:
     	if tom.user.id == tom_id and (tom.status == enums.ChatMemberStatus.OWNER or tom.status == enums.ChatMemberStatus.ADMINISTRATOR):
     		client.promote_chat_member(chat_id, user_id, ToM)
-    		message.reply(f"تم رفع {user_id} ادمن بنجاح")
+    		message.reply(f"")
