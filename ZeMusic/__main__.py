755d400d262043774ffa509939528025ee790449
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-26 01:17:34 +0200

    Update __main__.py

--- ZeMusic/__main__.py
+++ ZeMusic/__main__.py
@@ -11,7 +11,6 @@ from ZeMusic.misc import sudo
 from ZeMusic.plugins import ALL_MODULES
 from ZeMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
-from ZeMusic.plugins.play.azan import azkar
 
 
 async def init():
