2d63ea201697dee019b9b17a1b7f7ade5bd7b701
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-26 02:53:24 +0200

    Update همسه.py

--- ZeMusic/plugins/play/همسه.py
+++ ZeMusic/plugins/play/همسه.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import enums
 from pyrogram import types
+from pyrogram import filters
 from ZeMusic import app
 
 hmses = {}
