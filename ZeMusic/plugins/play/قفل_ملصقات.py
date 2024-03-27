335dacf023699232a61d11f39cde6d0e0d645828
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-27 23:03:54 +0200

    Update قفل_ملصقات.py

--- ZeMusic/plugins/play/قفل_ملصقات.py
+++ ZeMusic/plugins/play/قفل_ملصقات.py
@@ -2,39 +2,32 @@ from strings.filters import command
 from ZeMusic import app
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.enums import ParseMode, ChatMemberStatus
-stiklok =[]
+from pyrogram.enums import ChatMemberStatus
+stiklok = []
 
-@app.on_message(
-    filters.command(["قفل الملصقات","تعطيل الملصقات"])
- 
-   
-)
+@app.on_message(filters.text & filters.regex(r'^(/|!|)قفل الملصقات$'))
 async def block_stickers(client:Client, message:Message):
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
     if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
         if message.chat.id in stiklok:
-            return await message.reply_text(f"يا {message.from_user.mention} الملصقات مقفله من قبل")
+            return await message.reply_text(f"يا {message.from_user.mention} الملصقات مقفلة من قبل")
         stiklok.append(message.chat.id)
         return await message.reply_text(f"تم قفل الملصقات \n\n من قبل ←{message.from_user.mention}")
     else:
         return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا")
-    
-    
-    
 
-@app.on_message(
-    filters.command(["فتح الملصقات","تفعيل الملصقات"])
- 
-   
-)
-async def block_stickers(client:Client, message:Message):
+@app.on_message(filters.text & filters.regex(r'^(/|!|)فتح الملصقات$'))
+async def unblock_stickers(client:Client, message:Message):
     get = await client.get_chat_member(message.chat.id, message.from_user.id)
-    if get.status in ["creator", "administrator"]:
-        if message.chat.id in stiklok:
-            return await message.reply_text(f"يا {message.from_user.mention} الملصقات مقفله من قبل")
-        stiklok.append(message.chat.id)
-        return await message.reply_text(f"تم قفل الملصقات \n\n من قبل ←{message.from_user.mention}")
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id not in stiklok:
+            return await message.reply_text(f"يا {message.from_user.mention} الملصقات مفتوحة من قبل")
+        stiklok.remove(message.chat.id)
+        return await message.reply_text(f"تم فتح الملصقات \n\n من قبل ←{message.from_user.mention}")
     else:
         return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا")
-    
+
+@app.on_message(filters.sticker)
+async def delete_stickers(client:Client, message:Message):
+    if message.chat.id in stiklok:
+        await message.delete()
