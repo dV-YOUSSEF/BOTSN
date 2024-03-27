4b377bcdaaa4ce5222613cbbdcd44325c2852c7c
dV-YOUSSEF <162756525+dV-YOUSSEF@users.noreply.github.com>
2024-03-27 23:13:39 +0200

    Update قفل_ملصقات.py

--- ZeMusic/plugins/play/قفل_ملصقات.py
+++ ZeMusic/plugins/play/قفل_ملصقات.py
@@ -31,3 +31,31 @@ async def unblock_stickers(client:Client, message:Message):
 async def delete_stickers(client:Client, message:Message):
     if message.chat.id in stiklok:
         await message.delete()
+
+
+@app.on_message(filters.text & filters.regex(r'^(/|!|)قفل الصور$'))
+async def block_photos(client:Client, message:Message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id in photos_lock:
+            return await message.reply_text(f"يا {message.from_user.mention} الصور مقفلة من قبل")
+        photos_lock.append(message.chat.id)
+        return await message.reply_text(f"تم قفل الصور \n\n من قبل ←{message.from_user.mention}")
+    else:
+        return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا")
+
+@app.on_message(filters.text & filters.regex(r'^(/|!|)فتح الصور$'))
+async def unblock_photos(client:Client, message:Message):
+    get = await client.get_chat_member(message.chat.id, message.from_user.id)
+    if get.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+        if message.chat.id not in photos_lock:
+            return await message.reply_text(f"يا {message.from_user.mention} الصور مفتوحة من قبل")
+        photos_lock.remove(message.chat.id)
+        return await message.reply_text(f"تم فتح الصور \n\n من قبل ←{message.from_user.mention}")
+    else:
+        return await message.reply_text(f"يا {message.from_user.mention} انت لست مشرفا")
+
+@app.on_message(filters.photo)
+async def delete_photos(client:Client, message:Message):
+    if message.chat.id in photos_lock:
+        await message.delete()
